a74a8369b6e499f17275368261742dd5b1b9cbf4
ashutosh215 <mailit.ashutosh@gmail.com>
2019-11-03 04:01:53 +0530

    Update search_display.php

--- includes/search_display.php
+++ includes/search_display.php
@@ -4,6 +4,9 @@
   <title></title>
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="/resources/demos/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
   <script>
@@ -12,29 +15,39 @@
   } );
   </script>
 </head>
+<div class="container">
+  <?php
+  include 'db_connect.php';
+  $sql = "SELECT jokeId, jokeQues, jokeAns FROM jokes_table";
+  $result = $conn->query($sql);
+  ?>
+  <div class="row">
+    <div class="col col-12">
+      <header>
+        <h2>All the jokes that i know!</h2>
+      </header>
+    </div>
+  </div>
+  <div id="accordion">
 
-<?php
-include 'db_connect.php';
-$sql = "SELECT jokeId, jokeQues, jokeAns FROM jokes_table";
-$result = $conn->query($sql);
-?>
+  <?php
 
-<div id="accordion">
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+          echo '<h3>';
+          echo $row["jokeQues"];
+          echo '</h3>';
+          echo '<div><p>';
+          echo $row["jokeAns"];
+          echo '</p></div>';
+      }
+  } else {
+      echo "0 results";
+  }
 
-<?php
+  ?>
+  </div>
+    <a href="../index.php" class="btn btn-primary btn-primary"><span class="glyphicon glyphicon-arrow-left"></span> Back?</a>
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo '<h3>';
-        echo $row["jokeQues"];
-        echo '</h3>';
-        echo '<div><p>';
-        echo $row["jokeAns"];
-        echo '</p></div>';
-    }
-} else {
-    echo "0 results";
-}
-
-?>
+</div>
