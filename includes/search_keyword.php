cc648a98acc7d6b7c1cd7ef1944e3a69c186f0e3
ashutosh215 <mailit.ashutosh@gmail.com>
2019-11-02 22:35:27 +0530

    Add files via upload

---
+++ includes/search_keyword.php
@@ -0,0 +1,45 @@
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+  </script>
+</head>
+<?php
+
+include "db_connect.php";
+$keywordinform = $_GET["keyword"];
+$keywordinform = addslashes($keywordinform);
+echo "<h2>SEARCH FOR KEYWORD IN JOKE</h2>";
+$sql = "SELECT jokeId, jokeQues, jokeAns FROM jokes_table WHERE jokeQues LIKE '%$keywordinform%'";
+$result = $conn->query($sql);
+?>
+
+<div id="accordion">
+
+<?php
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+     
+        echo '<h3>';
+        echo $row["jokeQues"];
+        echo '</h3>';
+        echo '<div><p>';
+        echo $row["jokeAns"];
+        echo '</p></div>';
+    }
+} else {
+    echo "0 results";
+}
+
+?>
+</div>
